d6b48d86f57087d9430edfc0f5d0626254710e5a
mashaohua <mashaohua@autohome.com.cn>
2022-10-27 19:09:38 +0800

    广告系统添加eureka集群服务

---
+++ ad-eureka/src/main/java/com/shawntime/ad/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.shawntime.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author mashaohua
+ * @date 2022/10/27 17:52
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
